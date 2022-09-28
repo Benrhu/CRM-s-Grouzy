ab4088fb8ef09d34104be463570eb8552086c3e0
Rubdh <Benrhu@gmail.com>
2022-09-28 17:05:25 +0200

    Front and Customer structure

---
+++ src/main/java/com/grouzy/crm/services/CustomerService.java
@@ -0,0 +1,27 @@
+package com.grouzy.crm.services;
+
+import com.grouzy.crm.repositories.CustomerRepository;
+import com.grouzy.crm.specifications.CustomerDatatableFilter;
+import com.grouzy.crm.entities.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Page<Customer> getCustomersForDatatable(String queryString, Pageable pageable) {
+
+        CustomerDatatableFilter customerDatatableFilter = new CustomerDatatableFilter(queryString);
+
+        return customerRepository.findAll(customerDatatableFilter, pageable);
+    }
+}
---
+++ src/test/java/com/grouzy/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouzy.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
